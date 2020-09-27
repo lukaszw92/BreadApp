6698cf6c1fb30dcc5c3202d7d306ef4d748748f2
lukaszw92 <lukasz.wieczorek@op.pl>
2020-09-27 14:56:26 +0200

    modified user creation default group

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User, Group
 from django.views.generic import UpdateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # class LoginView(View):
 #     def get(self, request):
@@ -17,8 +20,17 @@ class CreateUserView(CreateView):
     success_url = reverse_lazy('login')
     template_name = "registration/new_user.html"
 
-    def form_valid(self, form):
-        ret_val = super().form_valid(form)
-        client_group = Group.objects.get(name='regular_users')
-        self.object.groups.add(client_group)
-        return ret_val
+    from django.db.models.signals import post_save
+    from django.dispatch import receiver
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        instance.groups.add(Group.objects.get(name='regular_users'))
+
+    # def form_valid(self, form):
+    #     ret_val = super().form_valid(form)
+    #     client_group = Group.objects.get(name='regular_users')
+    #     self.object.groups.add(client_group)
+    #     return ret_val
